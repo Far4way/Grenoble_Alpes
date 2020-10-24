207c8f960c3ba11904da46da399acda74d5deb2d
Florian <florian.laffont@grenoble-inp.org>
2020-10-24 13:55:02 +0200

    Removal of unused files

--- web_server/hello.py
+++
@@ -1,9 +0,0 @@
-import sys
-counter=0
-
-while True:
-    line = sys.stdin.readline()
-    if line :
-        stringueu = line.strip() + str(counter)
-        print(stringueu,flush=True)
-        counter+=1
