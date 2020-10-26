076f6421513b6e34375b6d713c0ec5a72f034856
Mab00 <73484763+Mab00@users.noreply.github.com>
2020-10-26 16:41:22 +0100

    Update CommMotor.ino

--- arduinoPrograms/CommMotor/CommMotor.ino
+++ arduinoPrograms/CommMotor/CommMotor.ino
@@ -1,9 +1,9 @@
 #include <Hermes.h>
-#include <Move_plate_adapted.h>
+#include <Move_plate.h>
 
 
-Move_plate_adapted Mot;
-Hermes herm(0b001,Mot);
+Move_plate Mot;
+Hermes herm(0b010,Mot);
 
 void setup() {
   herm.setup_OpClass();
