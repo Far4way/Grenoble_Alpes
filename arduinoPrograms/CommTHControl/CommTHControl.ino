9c86d353e81019eb3186a90c32f1e10828127226
Mab00 <73484763+Mab00@users.noreply.github.com>
2020-10-26 16:44:57 +0100

    Update CommTHControl.ino

--- arduinoPrograms/CommTHControl/CommTHControl.ino
+++ arduinoPrograms/CommTHControl/CommTHControl.ino
@@ -1,10 +1,10 @@
-#include <Hermes.h>
+#include <HermAs.h>
 #include <THcontrol.h>
 
 
 
 THcontrol THC(50.0,0.01,10.0);
-Hermes herm(0b001,THC);
+HermAs herm(0b001,THC);
    
 void setup() {
   herm.setup_OpClass();
