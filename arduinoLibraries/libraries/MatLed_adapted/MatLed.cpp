61f122f860b3dea447a07650606e528dbf11f5cb
Mab00 <73484763+Mab00@users.noreply.github.com>
2020-10-26 16:56:40 +0100

    Update and rename MatLed_adapted.cpp to MatLed.cpp

--- arduinoLibraries/libraries/MatLed_adapted/MatLed.cpp
+++ arduinoLibraries/libraries/MatLed_adapted/MatLed.cpp
@@ -1,24 +1,19 @@
 
 #include "Arduino.h"
 #include <FastLED.h>
-#include "Hermes.h"
-#include "MatLed_adapted.h"
+#include "HermAs.h"
+#include "MatLed.h"
 
 //declaration des méthodes
 
 /*constructeur initiant les leds et les arguments*/
-MatLed_adapted::MatLed_adapted() : pin(PIN_MATLED), nbLedx(NB_LED_X), nbLedy(NB_LED_Y), red_default(0), green_default(0), blue_default(0), posX(0), posY(0)
+MatLed::MatLed() : pin(PIN_MATLED), nbLedx(NB_LED_X), nbLedy(NB_LED_Y), red_default(0), green_default(0), blue_default(0), posX(0), posY(0)
 {
   FastLED.addLeds<WS2812, PIN_MATLED>(Tableds, NB_LED_X*NB_LED_Y);
 }
 
-/*destructeur realisant rien*/
-MatLed_adapted::~MatLed_adapted(){
-  //delete[] Tableds; non allouer dynamiquement
-}
-
 /*affiche les leds dans l etat demande*/
-uint8_t MatLed_adapted::show(uint8_t notuse){
+uint8_t MatLed::show(uint8_t notuse){
   FastLED.show();
   return 1;
 }
@@ -26,7 +21,7 @@ uint8_t MatLed_adapted::show(uint8_t notuse){
 
 /*allumer 1 led en position (posX;posY), en RGB par default
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::RGB_1Led(uint8_t notuse){
+uint8_t MatLed::RGB_1Led(uint8_t notuse){
   uint8_t numLed = coord_to_num(posX, posY);
   if(numLed != -1){
     Tableds[numLed].setRGB( green_default, red_default, blue_default);
@@ -39,7 +34,7 @@ uint8_t MatLed_adapted::RGB_1Led(uint8_t notuse){
 
 /*etteind 1 led en position (posX;posY)
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::Black_1Led(uint8_t notuse){
+uint8_t MatLed::Black_1Led(uint8_t notuse){
   uint8_t numLed = coord_to_num(posX, posY);
   if(numLed != -1){
     Tableds[numLed] = CRGB::Black;
@@ -53,7 +48,7 @@ uint8_t MatLed_adapted::Black_1Led(uint8_t notuse){
 
 /*allumer 1 ligne de leds en position (posY), en RGB par default
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::RGB_ligne(uint8_t notuse){
+uint8_t MatLed::RGB_ligne(uint8_t notuse){
   if(posY>=0 && posY<nbLedy){
     for(uint8_t i=0; i<nbLedx; i++){
       Tableds[i+posY*nbLedx].setRGB( green_default, red_default, blue_default);
@@ -67,7 +62,7 @@ uint8_t MatLed_adapted::RGB_ligne(uint8_t notuse){
 
 /*etteind 1 ligne de leds en position (posY)
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::Black_ligne(uint8_t notuse){
+uint8_t MatLed::Black_ligne(uint8_t notuse){
   if(posY>=0 && posY<nbLedy){
     for(uint8_t i=0; i<nbLedx; i++){
       Tableds[i+posY*nbLedx] = CRGB::Black;
@@ -82,7 +77,7 @@ uint8_t MatLed_adapted::Black_ligne(uint8_t notuse){
 
 /*allumer 1 colonne de leds, en position (posX), en RGB par default
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::RGB_column(uint8_t notuse){
+uint8_t MatLed::RGB_column(uint8_t notuse){
   if(nbLedx>=0 && posX<nbLedx){
     for(uint8_t  i=0; i<nbLedy; i++){
       if(i%2==0) Tableds[posX+i*nbLedx].setRGB( green_default, red_default, blue_default);
@@ -97,7 +92,7 @@ uint8_t MatLed_adapted::RGB_column(uint8_t notuse){
 
 /*etteind 1 colonne de leds, en position (posX)
 retourne 0 si echec, 1 si réussite*/
-uint8_t MatLed_adapted::Black_column(uint8_t notuse){
+uint8_t MatLed::Black_column(uint8_t notuse){
   if(posX>=0 && posX<nbLedx){
     for(uint8_t i=0; i<nbLedy; i++){
       if(i%2==0) Tableds[posX+i*nbLedx] = CRGB::Black;
@@ -111,7 +106,7 @@ uint8_t MatLed_adapted::Black_column(uint8_t notuse){
 }
 
 /*allume toutes les leds, en RGB par defaulte*/
-uint8_t MatLed_adapted::RGB_all(uint8_t notuse){
+uint8_t MatLed::RGB_all(uint8_t notuse){
   for(uint8_t i=0; i<nbLedy*nbLedx; i++){
     Tableds[i].setRGB( green_default, red_default, blue_default);
   }
@@ -120,7 +115,7 @@ uint8_t MatLed_adapted::RGB_all(uint8_t notuse){
 }
 
 /*etteint toutes les leds*/
-uint8_t MatLed_adapted::Black_all(uint8_t notuse){
+uint8_t MatLed::Black_all(uint8_t notuse){
   for(uint8_t i=0; i<nbLedy*nbLedx; i++){
     Tableds[i] = CRGB::Black;
   }
@@ -130,7 +125,7 @@ uint8_t MatLed_adapted::Black_all(uint8_t notuse){
 
 
 /*fixe la couleur par default pour obtenir la couleur de l arc en ciel correspondant au numero*/
-uint8_t MatLed_adapted::coord_to_num(uint8_t posX, uint8_t posY){
+uint8_t MatLed::coord_to_num(uint8_t posX, uint8_t posY){
   if(posX>=0 && posX<nbLedx && posY>=0 && posY<nbLedy){
     if(posY%2==0) return posX+posY*nbLedx;
     if(posY%2==1) return nbLedx-posX-1+posY*nbLedx;
@@ -139,7 +134,7 @@ uint8_t MatLed_adapted::coord_to_num(uint8_t posX, uint8_t posY){
 }
 
 
-void MatLed_adapted::SetNbArgMeth(){
+void MatLed::SetNbArgMeth(){
 	nbArgFloat = 0;
 
 	nbArgInt8 = 8;
@@ -152,7 +147,7 @@ void MatLed_adapted::SetNbArgMeth(){
 	nbMethod = 9;	
 }
 
-void MatLed_adapted::SetArgMeth_inTab(){
+void MatLed::SetArgMeth_inTab(){
 
 	//uint8_t
 	Tabi8[0] = &pin;
@@ -176,6 +171,6 @@ void MatLed_adapted::SetArgMeth_inTab(){
 	TabMeth[8] = (OperatingClass::ptrf) &MatLed_adapted::Black_all;
 }
 
-void MatLed_adapted::Setup(){
+void MatLed::Setup(){
 
 }
